9706ec13d64b87876e56c479071dfd2efa025eda
Aaron <aaron@storrer.net>
2022-01-12 14:14:19 -0700

    Completed Example 3

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,7 +2,22 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
+  },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num
+    },
+    remove(num){
+      this.counter = this.counter - num
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    submitForm(event) {
+      alert('Submitted!');
+    }
   }
 });
 
